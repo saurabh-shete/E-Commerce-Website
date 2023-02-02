399bc150af38b059e848ae6bb4bc821b9dfa811b
saurabh-shete <ss.saurabhshete@gmail.com>
2023-02-02 23:47:05 +0530

    final changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import { Container } from 'react-bootstrap';
-import { BrowserRouter as Router, Route ,Routes} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
@@ -16,25 +16,25 @@ import OrderScreen from "./screens/OrderScreen";
 const App = () => {
   return (
     <Router>
-    <Header/>
-    <main className='my-3'>
-      <Container>
+      <Header />
+      <main className='my-3'>
+        <Container>
           <Routes>
             <Route exact path="/" element={<HomeScreen />} />
             <Route path="/product/:id" element={<ProductDetails />} />
             <Route path="/cart/:id/:qty" element={<CartScreen />} />
             <Route path="/cart" element={<CartScreen />} />
-            <Route path="/login" element={<LoginScreen/>}/>
-            <Route path="/register" element={<RegisterScreen/>}/>
-            <Route path="/profile" element={<ProfileScreen/>}/>
-            <Route path="/shipping" element={<ShippingScreen/>}/>
-            <Route path="/payment" element={<PaymentScreen/>}/>
-            <Route path="/placeorder" element={<PlaceOrderScreen/>}/>
-            <Route path="/order/:_id" element={<OrderScreen/>}/>
-        </Routes>
-      </Container>
-    </main>
-    <Footer/>
+            <Route path="/login" element={<LoginScreen />} />
+            <Route path="/register" element={<RegisterScreen />} />
+            <Route path="/profile" element={<ProfileScreen />} />
+            <Route path="/shipping" element={<ShippingScreen />} />
+            <Route path="/payment" element={<PaymentScreen />} />
+            <Route path="/placeorder" element={<PlaceOrderScreen />} />
+            <Route path="/order/:_id" element={<OrderScreen />} />
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
     </Router>
   );
 }
